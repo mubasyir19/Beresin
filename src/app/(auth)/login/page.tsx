ac38d90fa1b1e21097103b5b45ea27478a5bf4eb
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-05-29 02:32:13 +0700

    add: hook login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,28 +1,40 @@
 import InputField from "@/components/molecules/InputField";
+import { useLogin } from "@/hooks/auth/useLogin";
 import Link from "next/link";
 import React from "react";
 
 export default function LoginPage() {
+  const { formData, handleChange, handleSubmit } = useLogin();
   return (
     <div className="flex h-screen w-full items-center justify-center bg-dark-blue">
       <div className="size-64 w-96">
         <h1 className="text-center text-2xl font-bold uppercase text-indigo-600">
           Beresin
         </h1>
-        <form className="mt-5 flex w-full flex-col gap-2 rounded-xl border border-secondary-3 bg-secondary-1 p-8">
+        <form
+          onSubmit={handleSubmit}
+          className="mt-5 flex w-full flex-col gap-2 rounded-xl border border-secondary-3 bg-secondary-1 p-8"
+        >
           <InputField
             type="text"
             label="Username"
-            placeholder="Ketik Username"
             name="username"
+            value={formData.username}
+            onChange={handleChange}
+            placeholder="Ketik Username"
           />
           <InputField
             type="password"
             label="Password"
-            placeholder="Ketik password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Ketik password"
           />
-          <button className="w-full rounded-md bg-indigo-500 px-4 py-2 text-center text-base font-semibold text-white hover:bg-indigo-600">
+          <button
+            type="submit"
+            className="w-full rounded-md bg-indigo-500 px-4 py-2 text-center text-base font-semibold text-white hover:bg-indigo-600"
+          >
             Submit
           </button>
         </form>
---
+++ src/hooks/auth/useLogin.ts
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export function useLogin() {
+  const router = useRouter();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BERESIN}/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        const tokenBase64 = btoa(data.access_token);
+        Cookies.set("appToken", tokenBase64, {
+          expires: 7,
+          secure: true,
+          sameSite: "strict",
+        });
+      } else {
+        router.push("/login");
+      }
+    } catch (error) {
+      toast.error("failed login");
+      console.log(error);
+    }
+  };
+
+  return {
+    formData,
+    handleChange,
+    handleSubmit,
+  };
+}
