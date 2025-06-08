7f8e0c1183c0f6ab6784ff85b3c9e0273bd7217e
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-06-08 22:25:19 +0700

    update: font layout auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "../globals.css";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.className}`}>
         <Toaster richColors position="top-right" />
         {children}
       </body>
